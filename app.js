5885f62f4492f7abba7606e656fc4b926e7f27de
pascalsfan <harryalfred492@gmail.com>
2024-05-14 12:09:06 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+
+function display(){
+
+
+}
+const become= document.getElementById('become')
+become.addEventListener('click', function(){
+    window.onload = function() {
+        $('#vipmodal').modal('show');
+      };
+      alert('Please do well to contact our email by click on the navigational icon if you want to become a fan of Pascal Pedro Thanks for your understanding or the email on this Pop up mail.duncanmillership@gmail.com')
+})
